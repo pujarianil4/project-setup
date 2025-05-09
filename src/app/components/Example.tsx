311626580d0f6dc6189b22d8f1376c340115f35c
Anil Pujari <pujarianil4@gmail.com>
2025-05-09 12:31:32 +0530

    lint and prettier setup

---
+++ src/app/components/Example.tsx
@@ -0,0 +1,20 @@
+interface IExampleProps {
+  name: string;
+  isActive: boolean;
+}
+
+export const Example = ({ name, isActive }: IExampleProps) => {
+  const greeting = 'Hello';
+  const message = `${greeting} ${name}`;
+
+  if (isActive) {
+    console.warn(message);
+  }
+
+  return (
+    <div className='container'>
+      <span>{message}</span>
+      <br />
+    </div>
+  );
+};
